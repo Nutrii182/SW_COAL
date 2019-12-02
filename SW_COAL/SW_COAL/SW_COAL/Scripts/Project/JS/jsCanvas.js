ddcbfeb36a8ce17467f5e5ce80eb295204f3ad10
nutrii182 <nutrii182@gmail.com>
2019-12-01 21:55:53 -0600

    Imagen cargada con canvas

---
+++ SW_COAL/SW_COAL/SW_COAL/Scripts/Project/JS/jsCanvas.js
@@ -0,0 +1,27 @@
+﻿function LoadContextCanvas(idCanvas) {
+
+    var element = document.getElementById(idCanvas);
+
+    if (element && element.getContext) {
+        var context = element.getContext('2d');
+        if (context)
+            return context;
+    }
+    return false;
+}
+
+window.onload = function () {
+
+    var ctx = LoadContextCanvas('miCanvas');
+
+    if (ctx) {
+
+        var img = new Image();
+
+        img.src = "/Content/img/paradontograma.png";
+
+        img.onload = function () {
+            ctx.drawImage(img, 10, 10);
+        }
+    }
+}
